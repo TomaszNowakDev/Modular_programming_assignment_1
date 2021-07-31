5ca7c93cda89051c43bd503c957070945b86acef
Tomasz Nowak <tomeknew1@gmail.com>
2021-07-31 21:00:57 +0100

    Variables teacher_name, points_for_table and earned_points created. Functions teacher_name_and_points() and points_earned() created and called.

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import functions
 
 
+def teacher_name_and_points():
+    name = functions.read_non_empty_string("Name of the Teacher ==> ")
+    points = functions.read_positive_integer("Number of points ==> ")
+    return name, points
+
+
+def points_earned(table_name):
+    points = []
+    for i in range(len(table_name)):
+        x = functions.read_positive_integer(f'How many points {table_name[i]} earned?\n==>')
+        points.append(x)
+    return points
+
+
 def main():
     print("Tomasz Nowak")
     tables = ["Glas", "Dubh", "Gorm", "Dearg"]
+    teacher_name, points_for_table = teacher_name_and_points()
+    earned_p = points_earned(tables)
 
 
 if __name__ == '__main__':
